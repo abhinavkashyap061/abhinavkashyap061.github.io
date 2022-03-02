ef703216a182a1df39ed233e20313f0b511f03eb
Abhinav Kashyap <84775901+abhinavkashyap061@users.noreply.github.com>
2022-03-02 19:27:29 +0530

    scrollUp button updated

--- scripts/upward-button.js
+++ scripts/upward-button.js
@@ -10,7 +10,7 @@ function myFunction() {
     }
 };
 
-function topFunction() {
+myButton.addEventListener('click', function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-};
+});
