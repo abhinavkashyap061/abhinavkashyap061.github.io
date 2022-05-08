c760f817c7250238e6decdc50c150959df9d4c55
Abhinav <abhinavkashyap061@gmail.com>
2022-05-08 11:13:46 +0530

    hackerman easter egg added

--- scripts/dumb-easter-egg.js
+++ scripts/dumb-easter-egg.js
@@ -1,11 +1,18 @@
 
 const btn = document.querySelector("#dumb-easter-egg");
 const text = document.querySelector("#easter-egg-text");
+let img = document.querySelector("#easter-egg-img");
 
 let count = 0;
 btn.addEventListener("click", function(){
     count++;
-    if(count%5==0){
+    if(count==10 || count ==30){
+        img.style.display = "inline-block";
+        setInterval(function(){
+            img.style.display = "none";
+        },
+        10000);
+    }else if(count%5==0){
         text.innerHTML = "you clicked me " + count + " times!";
         alert("chill !!!");
     }else{
