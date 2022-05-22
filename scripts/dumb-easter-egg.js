3f09dd16c563a7cf4227f5d605d51853f150679e
Abhinav <abhinavkashyap061@gmail.com>
2022-05-23 01:54:51 +0530

    souped up easter egg

--- scripts/dumb-easter-egg.js
+++ scripts/dumb-easter-egg.js
@@ -3,19 +3,25 @@ const btn = document.querySelector("#dumb-easter-egg");
 const text = document.querySelector("#easter-egg-text");
 let img = document.querySelector("#easter-egg-img");
 
+//easter egg audio
+let truthSed = new Audio("assets/easter-egg.mp3");
+
+
 let count = 0;
 btn.addEventListener("click", function(){
     count++;
     if(count==10 || count ==30){
+        
+        //audio playing
+        truthSed.play();
+        
+        //pepe dancing
         img.style.display = "inline-block";
         setInterval(function(){
             img.style.display = "none";
         },
         10000);
-    }else if(count%5==0){
-        text.innerHTML = "you clicked me " + count + " times!";
-        alert("chill !!!");
     }else{
-        text.innerHTML = "you clicked me " + count + " times!";
+        text.innerHTML = "<p>click 10 times to know the truth of my life 🥺</p>" + "you clicked me " + count + " times!";
     }
 })
